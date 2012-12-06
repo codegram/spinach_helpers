69787203330134b754d6fb48b3ea0afbf61a4f2c
Oriol Gual <oriol.gual@gmail.com>
2012-12-06 06:27:11 -0800

    Create current_user.rb

---
+++ current_user.rb
@@ -0,0 +1,20 @@
+#
+# Usage: 
+# 
+#   class MyFeature < Spinach::FeatureSteps
+#     include Spinach::CurrentUser
+#   end
+#
+module Spinach
+  module CurrentUser
+    include Spinach::DSL
+
+    def current_user
+      # Fabrication
+      # @current_user ||= Fabricate(:user, password: 'test1234', password_confirmation: 'test1234')
+
+      # Factory Girl
+      # @current_user ||= FactoryGirl.create(:user, password: 'test1234', password_confirmation: 'test1234')
+    end
+  end
+end
